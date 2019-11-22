26ae9314b6d4446c6a2e6ca42ce36450ea2e052c
calliejh <57910979+calliejh@users.noreply.github.com>
2019-11-22 11:32:43 -0500

    Update teamc.py

--- teamc.py
+++ teamc.py
@@ -163,3 +163,114 @@ def tree():
         c.forward(1)
         c.left(1)
     c.end_fill()
+    
+def drawOneFish():
+    #by Callie
+    c.fillcolor(randint(0,255),randint(0,255),randint(0,255)) #random fill color
+    c.begin_fill()
+    c.left(60)
+    number = randint(10,100)
+    number = number/100 #divides the number by 100 to make the fish body smaller
+    for i in range(120):
+        c.forward(number)
+        c.right(1)
+    c.right(60)
+    for i in range(120):
+        c.forward(number) #Goes to position to make the tail
+        c.right(1)
+    c.right(60)
+    for i in range(120):
+        c.forward(number)
+        c.right(1)
+    c.forward(30*number) #draws fish tail (makes the tail proportional to the fish body)
+    c.left(150)
+    c.forward(50*number)
+    c.left(150)
+    c.forward(30*number)
+    c.end_fill()
+    c.right(60)
+
+def drawOneSeaweed():
+    #by Callie
+    c.pencolor("light green")
+    c.fillcolor("light green")
+    c.begin_fill()
+    diameter = randint(10,30) #make a semicircle
+    for i in range(randint(3,10)):
+        c.circle((diameter/2), 180,)
+        c.left(90)
+        c.forward(diameter)
+        c.forward(-diameter)
+        c.right(90)
+        c.circle((-diameter/2), 180) #make another semicircle but in the opposite direction so that it does not form a complete circle
+        c.right(90)
+        c.forward(diameter)
+        c.forward(-diameter)
+        c.left(90)
+        position = c.ycor()
+    if position > 295: #stop seaweed if it goes above the waves
+        c.penup()
+        c.end_fill()
+    c.end_fill()
+
+def sunWithRays():
+    #by Callie
+    c.pencolor("yellow")
+    c.pensize(10)
+    c.fillcolor("yellow")
+    c.begin_fill()
+    c.pendown()
+    for i in range(400):
+        c.forward(2)
+        c.left(1)
+    c.end_fill()
+    c.penup()
+    c.goto(0, -20)
+    c.pendown()
+    c.right(130)
+    c.forward(50)
+    c.penup()
+    c.goto(150, -20)
+    c.pendown()
+    c.right(140)
+    c.forward(50)
+    c.penup()
+    c.goto(150, 100)
+    c.pendown()
+    c.right(140)
+    c.forward(50)
+    c.penup()
+    c.goto(150, 220)
+    c.pendown()
+    c.right(130)
+    c.forward(50)
+    c.penup()
+    c.goto(0, 250)
+    c.pendown()
+    c.right(130)
+    c.forward(50)
+    c.penup()
+    c.goto(-150, 250)
+    c.pendown()
+    c.right(140)
+    c.forward(50)
+    c.penup()
+    c.goto(-150, 100)
+    c.pendown()
+    c.right(140)
+    c.forward(50)
+    c.penup()
+    c.goto(-150, -20)
+    c.pendown()
+    c.right(130)
+    c.forward(50)
+
+def drawStarFish():
+    c.pendown()
+    c.pencolor("pink")
+    c.begin_fill()
+    c.fillcolor("pink")
+    for i in range(5): #draw star shape
+        c.forward(40)
+        c.right(144)
+    c.end_fill()
